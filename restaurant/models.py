5b315b88e881c4ef0a3f0a9d56f847496dc27729
Palash Sarkar <palash.sarkar@protonmail.com>
2021-03-13 20:51:47 +0200

    Use keys for open and close strings

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,6 +3,8 @@ from typing import List
 from dacite import from_dict, exceptions
 from .utils import convert_seconds_to_hours
 
+OPEN_KEY = "open"
+CLOSE_KEY = "close"
 
 @dataclass
 class TimeInfo:
@@ -62,7 +64,7 @@ def convert_to_human_readable(opening_hours_obj: OpeningHours) -> str:
         time_list = getattr(opening_hours_obj, day)
         if len(time_list) == 1 and not time_list[0].type:
             pass
-        elif len(time_list) == 1 and time_list[0].type == "close":
+        elif len(time_list) == 1 and time_list[0].type == CLOSE_KEY:
             pass
         elif len(time_list) == 0:
             output += f"{day.capitalize()}: Closed\n"
@@ -96,32 +98,32 @@ def process_opening_hours_on_a_day(day: str,
     """
     day_output = f"{day.capitalize()}:"
     time_list.sort(key=lambda timing: timing.value)
-    next_expected_type = "close"
+    next_expected_type = CLOSE_KEY
     for j in range(0, len(time_list)):
         time_obj = time_list[j]
-        if j == 0 and time_obj.type == "close":
+        if j == 0 and time_obj.type == CLOSE_KEY:
             pass
         elif time_obj.type == next_expected_type:
             raise ValueError(f"Two consecutive {time_obj.type} time.")
-        elif time_obj.type == "close":
+        elif time_obj.type == CLOSE_KEY:
             day_output += f" - {time_obj.convert_epoch_to_human_readable()},"
             next_expected_type = time_obj.type
         elif j == len(time_list) - 1 \
-                and time_list[j].type == "open" \
+                and time_list[j].type == OPEN_KEY \
                 and next_day_first_time_info is not None \
-                and next_day_first_time_info.type == "close":
+                and next_day_first_time_info.type == CLOSE_KEY:
             day_output += f" {time_obj.convert_epoch_to_human_readable()} - " \
                           f"{next_day_first_time_info.convert_epoch_to_human_readable()},"
             next_expected_type = time_obj.type
         elif j == len(time_list) - 1 \
-                and time_list[j].type == "open" \
+                and time_list[j].type == OPEN_KEY \
                 and next_day_first_time_info is None:
             raise ValueError(f"Expecting closing time for {day} on  the next day, "
                              f"but no such closing time specified.")
-        elif j == len(time_list) - 1 and time_list[j].type == "open":
+        elif j == len(time_list) - 1 and time_list[j].type == OPEN_KEY:
             raise ValueError(f"Expecting closing time for {day} on  the next day, "
                              f"but no such closing time specified.")
-        elif time_obj.type == "open":
+        elif time_obj.type == OPEN_KEY:
             day_output += f" {time_obj.convert_epoch_to_human_readable()}"
             next_expected_type = time_obj.type
         else:
