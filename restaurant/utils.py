3c24e00dade09ed1106622096d53b4939c775c78
Palash Sarkar <palash.sarkar@protonmail.com>
2021-03-26 13:29:07 +0200

    Use inbuilt datetime method for determining human readable time

--- restaurant/utils.py
+++ restaurant/utils.py
@@ -8,23 +8,10 @@ def convert_seconds_to_hours(ts: int) -> str:
     :param ts: UNIX time in milliseconds
     :return: Time in hh or hh:mm or hh:mm:ss
     """
-    ts_human = datetime.datetime.fromtimestamp(ts, pytz.utc)
-    hours = ts_human.hour
-    minutes = ts_human.minute
-    seconds = ts_human.second
-    if ts >= 43200:
-        am_pm = "PM"
-    else:
-        am_pm = "AM"
-    if hours > 12:
-        hours = hours - 12
-    minutes_str = "0" + str(minutes) if minutes < 10 else str(minutes)
-    seconds_str = "0" + str(seconds) if seconds < 10 else str(seconds)
-    if minutes == 0 and seconds == 0:
-        return f"{hours} {am_pm}"
-    elif minutes != 0 and seconds == 0:
-        return f"{hours}:{minutes_str} {am_pm}"
-    elif seconds != 0:
-        return f"{hours}:{minutes_str}:{seconds_str} {am_pm}"
-    else:
-        return f"{hours}:{minutes_str}:{seconds_str} {am_pm}"
+    time_format = "%-I:%M:%S %p"
+    if ts % 60 == 0:
+        time_format = "%-I:%M %p"
+    if ts % 3600 == 0:
+        time_format = "%-I %p"
+    ts_human_new = datetime.datetime.fromtimestamp(ts, pytz.utc).strftime(time_format)
+    return ts_human_new
